0f767fae696c9c2c1c47a6e33a30e1ff88529980
Graph Noor 247 <112159889+grapheart247@users.noreply.github.com>
2026-05-22 22:41:33 +0500

    Correct HTML entities in index.php
    Fix HTML entity encoding in Buy Me A Coffee widget script.

--- index.php
+++ index.php
@@ -531,7 +531,7 @@
 </script>
 
 <!-- Buy Me A Coffee Widget -->
-<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="grapheart365" data-description="Support me on Buy me a coffee!" data-mes[...]
+<script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="grapheart365" data-description="Support me on "Buy me a coffee!" " data-mes[...]
 
 </body>
 </html>
